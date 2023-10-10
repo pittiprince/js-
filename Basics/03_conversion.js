aada7a96219a214b7ac133575432339f419fbb0c
pittiprince <117570887+pittiprince@users.noreply.github.com>
2023-10-10 14:20:40 +0000

    commit

--- Basics/03_conversion.js
+++ Basics/03_conversion.js
@@ -1,15 +1,15 @@
 let age = "19" //value in string
-console.log(age) //print
-console.log(typeof(age)) //typeof 
+// console.log(age) //print
+// console.log(typeof(age)) //typeof 
 let age01 = Number(age) //conversion to Number 
-console.log(typeof(age01))
+// console.log(typeof(age01))
 
 let age0 = "19abc" //value in string not a complete number
-console.log(age0) //print
-console.log(typeof(age0)) //typeof 
+// console.log(age0) //print
+// console.log(typeof(age0)) //typeof 
 let age02 = Number(age0) //conversion to Number 
-console.log(typeof(age02))
-console.log(age02) //resulted NaN not a number 
+// console.log(typeof(age02))
+// console.log(age02) //resulted NaN not a number 
 
 
 //"33" => 33
@@ -18,3 +18,27 @@ console.log(age02) //resulted NaN not a number
 // 1=> true ; 0=> false [ boolean con]
 // "" => false ; "something"=>true 
 
+
+
+//*******************operations ***********
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue)
+
+//basic math operations
+ /*console.log(2+2)
+ console.log(2*2)
+ console.log(2-2)
+ console.log(2**2)
+ console.log(2/2)
+ console.log(2%2)*/
+
+ let str1= "hello !"
+ let str2 = " Prince"
+ let str3 = str1+str2 //str concat
+//  console.log(str3)
+
+console.log("1"+2+2) //if string is first then it converts rest to string
+console.log(1+2+"1") //if operations are first , then it will perform operation first and and string concat
+  
