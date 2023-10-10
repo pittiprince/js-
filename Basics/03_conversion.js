c233b1d3b5cd3d38ad627c83337e2965ef402777
pittiprince <117570887+pittiprince@users.noreply.github.com>
2023-10-10 10:51:40 +0000

    commit

---
+++ Basics/03_conversion.js
@@ -0,0 +1,20 @@
+let age = "19" //value in string
+console.log(age) //print
+console.log(typeof(age)) //typeof 
+let age01 = Number(age) //conversion to Number 
+console.log(typeof(age01))
+
+let age0 = "19abc" //value in string not a complete number
+console.log(age0) //print
+console.log(typeof(age0)) //typeof 
+let age02 = Number(age0) //conversion to Number 
+console.log(typeof(age02))
+console.log(age02) //resulted NaN not a number 
+
+
+//"33" => 33
+// "33abc" => NaN
+//true => 1; false = 0
+// 1=> true ; 0=> false [ boolean con]
+// "" => false ; "something"=>true 
+
