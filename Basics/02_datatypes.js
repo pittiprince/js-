d9ca02be83748012482fb5d2cf5b004cffd8c9d2
pittiprince <117570887+pittiprince@users.noreply.github.com>
2023-10-10 15:57:33 +0530

    Update 02_datatypes.js

--- Basics/02_datatypes.js
+++ Basics/02_datatypes.js
@@ -1,3 +1,21 @@
-"use strict"; //treat all js code as newer version
+//"use strict"; //treat all js code as newer version
 
-alert("hello") //we are using node js not browser
+//alert("hello") //we are using node js not browser
+
+
+let name = "Prince" //string|
+let age = 19 //number
+let isloggedIn = false // boolean
+
+
+//number
+//bigint
+//string
+//Boolean => true/False
+//null => empty [standalone value] is a object
+//undefined  is a object
+//symbol => uniqueness
+
+
+//object 
+console.log(typeof(null)); 
